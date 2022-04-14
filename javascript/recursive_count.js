a14e531a1221f1aab6e0308b00f851f95ab30dc3
gehrig_barnes <gehrigbarnes@gmail.com>
2022-04-14 09:51:28 -0500

    done

--- javascript/recursive_count.js
+++ javascript/recursive_count.js
@@ -1,6 +1,13 @@
 function recursiveCount(num = 0) {
   // type your code here
+  while (num < 10){
+    console.log(num);
+    ++num;
+    return recursiveCount(num);
+    
+  }
 }
+
 
 if (require.main === module) {
   recursiveCount();
